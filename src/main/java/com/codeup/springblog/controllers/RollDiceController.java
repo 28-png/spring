b9ec8a7a21fa0c8660622919ce35139a7241921c
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-03-16 10:31:29 -0500

    updated

--- src/main/java/com/codeup/springblog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -8,14 +8,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class RollDiceController {
 
-    @GetMapping("/roll-dice/{n}")
-    public String rollIt(@PathVariable int n, Model model) {
-        int num = (int) Math.random() * 6 + 1;
-        model.addAttribute("n", n);
-        if(n == num) {
-           return "Congrats!";
-        }
-        return "Wrong number!";
+    @GetMapping("/roll")
+    public String rollIt() {
+        return "roll";
+
     }
 
+    @GetMapping("/roll/{guess}")
+    public String rollDiceGuess(@PathVariable int guess, Model model) {
+        String message;
+        int random = (int) (Math.random() * 6) + 1;
+        if(random == guess) {
+            message = "you guessed random number";
+        } else {
+            message = "Sorry, try again.";
+        }
+        model.addAttribute("message", message);
+        model.addAttribute("guess", guess);
+        model.addAttribute("random", random);
+        return "roll-dice-guess";
+    }
 }
