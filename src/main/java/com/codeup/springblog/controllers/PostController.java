d5289661b36295290be8ebe3a7155aea872c87aa
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-03-18 15:52:21 -0500

    updated

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -2,6 +2,7 @@ package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.Posts;
 import com.codeup.springblog.model.Post;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -17,46 +18,47 @@ public class PostController {
 
     @GetMapping("/posts")
     public String getPosts(Model model) {
-        model.addAttribute("title", postsDao.findPostById(1L).getTitle());
-        model.addAttribute("postDate", postsDao.findPostById(1L).getDate());
-        model.addAttribute("body", postsDao.findPostById(1L).getBody());
+        model.addAttribute("posts", postsDao.findAll());
         return "posts";
     }
 
-//    @GetMapping("/posts/{id}")
-//    @ResponseBody
-//    public String getPost(@PathVariable int id) {
-//        return "view an individual post, id="+id;
-//    }
-//
-//    @GetMapping("/posts/create")
-//    @ResponseBody
-//    public String getCreatePostForm(){
-//        return "view the form for creating a post";
-//    }
-//
-//    @PostMapping("/posts/create")
-//    @ResponseBody
-//    public String createPost(){
-//        return "create a new post";
-//    }
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String getPost(@PathVariable int id) {
+        return "view an individual post, id="+id;
+    }
 
-//    @DeleteMapping("/posts/delete")
-//    public String delete(@RequestParam int id, Model model) {
-//        Post post = postsDao.findPostById(id);
-//        String deleteTitle = post.getTitle();
-//        Date deleteDate = post.getDate();
-//        String deleteBody = post.getBody();
-//        model.addAttribute("title", deleteTitle);
-//        model.addAttribute("postDate", deleteDate);
-//        model.addAttribute("body", deleteBody);
-//        postsDao.deleteById(id);
-//        return "posts";
-//    }
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String getCreatePostForm(){
+        return "view the form for creating a post";
+    }
 
-    @RequestMapping(value = "delete/{id}", method = RequestMethod.GET)
-    public String delete(@PathVariable("id") int id) {
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String createPost(){
+        return "create a new post";
+    }
+
+    @PostMapping("/posts/{id}/delete")
+    public String delete(@PathVariable long id){
         postsDao.deleteById(id);
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/posts/{id}/edit")
+    public String editForm(@PathVariable long id, Model model) {
+        Post postToEdit = postsDao.getOne(id);
+        model.addAttribute("post", postToEdit);
+        return "edit";
+    }
+    @PostMapping("/posts/{id}/edit")
+    public String updatePost(@PathVariable long id, @RequestParam String title, @RequestParam Date date ,@RequestParam String body) {
+        Post p = postsDao.getOne(id);
+        p.setTitle(title);
+        p.setDate(date);
+        p.setBody(body);
+        postsDao.save(p);
         return "redirect:/posts";
     }
 
--- src/main/java/com/codeup/springblog/model/Post.java
+++ src/main/java/com/codeup/springblog/model/Post.java
@@ -1,5 +1,7 @@
 package com.codeup.springblog.model;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
 import java.util.Date;
 
@@ -13,6 +15,7 @@ public class Post {
 @Column(nullable = false, length = 200, unique = true)
     private String title;
 @Column(nullable = false, name = "post_date", columnDefinition = "DATE")
+@DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date date;
 @Column(nullable = false, columnDefinition = "VARCHAR(255)")
     private String body;
