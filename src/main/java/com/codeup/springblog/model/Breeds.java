44a9bbed10e89cb7ef863ba2557a624315f5877c
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-03-26 11:59:43 -0500

    updated

--- src/main/java/com/codeup/springblog/model/Breeds.java
+++ src/main/java/com/codeup/springblog/model/Breeds.java
@@ -9,6 +9,51 @@ public class Breeds {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, columnDefinition = "INT(11) UNSIGNED")
     private long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    @ManyToOne
+    @JoinColumn(name="dog_id")
+    private Dogs dog;
 
+    public Breeds() {}
 
+    public Breeds(String name, String description, Dogs dog) {
+        this.name = name;
+        this.description = description;
+        this.dog = dog;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Dogs getDog() {
+        return dog;
+    }
+
+    public void setDog(Dogs dog) {
+        this.dog = dog;
+    }
 }
--- src/main/java/com/codeup/springblog/model/Dogs.java
+++ src/main/java/com/codeup/springblog/model/Dogs.java
@@ -1,10 +1,15 @@
 package com.codeup.springblog.model;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "dogs")
 public class Dogs {
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "dog")
+    private List<Breeds> breed;
+
     @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY)
     @Column(nullable = false, columnDefinition = "INT(11) UNSIGNED")
@@ -12,8 +17,17 @@ public class Dogs {
     @Column(nullable = false, columnDefinition = "VARCHAR(255)")
     private String group;
 
-    public Dogs(String group) {
+    public Dogs(List<Breeds> breed, String group) {
+        this.breed = breed;
         this.group = group;
+    }
+
+    public List<Breeds> getBreed() {
+        return breed;
+    }
+
+    public void setBreed(List<Breeds> breed) {
+        this.breed = breed;
     }
 
     public long getId() {
